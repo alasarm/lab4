ec706f39ff951a2376b93a87821b42ac564720b0
Arnie Sarmiento <asarmiento6@myseneca.ca>
2024-10-11 22:18:03 -0400

    Lab 4

---
+++ lab4d.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# Author ID: 120297221
+
+str1 = 'Hello World!!'
+str2 = 'Seneca College'
+
+num1 = 1500
+num2 = 1.50
+
+def first_five(string):
+    """Returns the first five characters of the string"""
+    return string[:5]
+
+def last_seven(string):
+    """Returns the last seven characters of the string"""
+    return string[-7:]
+
+def middle_number(number):
+    """Returns the second and third characters of the number as a string"""
+    str_num = str(number)
+    return str_num[1:3]
+
+def first_three_last_three(string1, string2):
+    """Returns a string made of the first three characters of string1 and last three characters of string2"""
+    return string1[:3] + string2[-3:]
+
+if __name__ == '__main__':
+    print(first_five(str1))  
+    print(first_five(str2))  
+    print(last_seven(str1))  
+    print(last_seven(str2))  
+    print(middle_number(num1))
+    print(middle_number(num2))  
+    print(first_three_last_three(str1, str2))  
+    print(first_three_last_three(str2, str1))  
+
